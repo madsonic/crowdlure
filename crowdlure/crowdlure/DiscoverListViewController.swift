fa628c13e421fbd7ace0fb08f0b3c1d6db4b6a29
Gerald <ngjingsiang@gmail.com>
2016-08-28 01:50:38 -0700

    Add search button to discover

--- crowdlure/crowdlure/DiscoverListViewController.swift
+++ crowdlure/crowdlure/DiscoverListViewController.swift
@@ -52,6 +52,7 @@ class DiscoverListViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "DISCOVER"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Search, target: nil, action: nil)
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.Plain, target:nil, action:nil)
         let view = UIView.init(frame: UIScreen.mainScreen().bounds)
         view.backgroundColor = UIColor.whiteColor()
